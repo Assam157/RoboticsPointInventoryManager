ffbcc051b90450f306c4e88e58cfde406d54c0a9
Assam157 <asasm8106@gmail.com>
2025-01-22 21:41:21 +0530

    Update Inventorypage.jsx

--- src/components/Inventorypage.jsx
+++ src/components/Inventorypage.jsx
@@ -10,7 +10,7 @@ const InventoryPage = () => {
         // Fetch all products from the backend
         const fetchProducts = async () => {
             try {
-                const response = await fetch('https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/get_products', {
+                const response = await fetch('https://roboticspointbackend-b6b7b2e85bbf.herokuapp.com/get_products', {
                     method: 'GET',
                     headers: { 'Content-Type': 'application/json' },
                 });
@@ -34,7 +34,7 @@ const InventoryPage = () => {
 
     const handleStockChange = async (productId, newQuantity) => {
         // Update the available quantity for the product
-        const response = await fetch('https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/update_product_stock', {
+        const response = await fetch('https://roboticspointbackend-b6b7b2e85bbf.herokuapp.com/update_product_stock', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ _id: productId, availableQuant: parseInt(newQuantity) }),
